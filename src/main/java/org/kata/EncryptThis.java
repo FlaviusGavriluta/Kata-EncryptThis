dce3426236fa0d4ebf00cb8e580753c7b51806a0
FlaviusGavriluta <flavius.gavriluta@gmail.com>
2023-07-27 10:06:02 +0300

    trying to remove .idea from github

--- src/main/java/org/kata/EncryptThis.java
+++ src/main/java/org/kata/EncryptThis.java
@@ -7,7 +7,8 @@ public class EncryptThis {
     public static String encryptThis(String text) {
         return Arrays.stream(text.split(" ")).
                 map(word -> word.length() > 2 ?
-                        (int) word.charAt(0) + word.substring(word.length() - 1) + word.substring(2, word.length() - 1) + word.charAt(1) :
+                        (int) word.charAt(0) + word.substring(word.length() - 1)
+                                + word.substring(2, word.length() - 1) + word.charAt(1) :
                         word.length() > 1 ?
                                 (int) word.charAt(0) + word.substring(1) :
                                 word.length() == 1 ? (int) word.charAt(0) + "" : "")
